5cba5792f2b80fdd1b8eb6d11bc4a38a96886f3d
wayloft <82181607+wayloft@users.noreply.github.com>
2024-08-22 20:54:23 -0500

    documentation and transform tool updates

--- Editor/CustomTransformTool.cs
+++ Editor/CustomTransformTool.cs
@@ -27,6 +27,8 @@ public class CustomTransformTool : EditorWindow
     private Color gizmoColor = Color.green;
     private float gizmoSize = 0.5f;
 
+    private Vector2 scrollPosition; // Scroll position variable
+
     [MenuItem("Tools/Custom Transform Tool")]
     public static void ShowWindow()
     {
@@ -35,23 +37,25 @@ public class CustomTransformTool : EditorWindow
 
     private void OnGUI()
     {
+        scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition); // Begin scroll view
+
         EditorGUILayout.LabelField("Transform Settings", EditorStyles.boldLabel);
 
-        position = EditorGUILayout.Vector3Field(new GUIContent("Position", "Set the position of the selected object(s)"), position);
-        rotation = EditorGUILayout.Vector3Field(new GUIContent("Rotation", "Set the rotation of the selected object(s)"), rotation);
-        scale = EditorGUILayout.Vector3Field(new GUIContent("Scale", "Set the scale of the selected object(s)"), scale);
+        position = EditorGUILayout.Vector3Field(new GUIContent("Position", "Set the position of the selected object(s). If grid snapping is enabled, this will snap to the nearest grid point."), position);
+        rotation = EditorGUILayout.Vector3Field(new GUIContent("Rotation", "Set the rotation of the selected object(s)."), rotation);
+        scale = EditorGUILayout.Vector3Field(new GUIContent("Scale", "Set the scale of the selected object(s)."), scale);
 
-        snapMode = (SnapMode)EditorGUILayout.EnumPopup(new GUIContent("Snap Mode", "Choose the snapping mode"), snapMode);
+        snapMode = (SnapMode)EditorGUILayout.EnumPopup(new GUIContent("Snap Mode", "Choose the snapping mode. Grid will snap to a defined grid size, while Vertex and Edge snapping allow for snapping to nearby vertices or edges."), snapMode);
 
         if (snapMode == SnapMode.Grid)
         {
-            snapToGrid = EditorGUILayout.Toggle(new GUIContent("Snap to Grid", "Enable snapping to grid"), snapToGrid);
+            snapToGrid = EditorGUILayout.Toggle(new GUIContent("Enable Grid Snapping", "Enable snapping to a grid."), snapToGrid);
             if (snapToGrid)
             {
                 snapX = EditorGUILayout.Toggle("Snap X", snapX);
                 snapY = EditorGUILayout.Toggle("Snap Y", snapY);
                 snapZ = EditorGUILayout.Toggle("Snap Z", snapZ);
-                gridSize = EditorGUILayout.FloatField(new GUIContent("Grid Size", "Set the size of the grid for snapping"), gridSize);
+                gridSize = EditorGUILayout.FloatField(new GUIContent("Grid Size", "Set the size of the grid. Objects will snap to the nearest grid lines based on this size."), gridSize);
             }
         }
 
@@ -63,14 +67,14 @@ public class CustomTransformTool : EditorWindow
         GUILayout.Space(10);
         EditorGUILayout.LabelField("Alignment & Distribution", EditorStyles.boldLabel);
 
-        alignMode = (AlignMode)EditorGUILayout.EnumPopup(new GUIContent("Align Mode", "Select how objects should be aligned"), alignMode);
+        alignMode = (AlignMode)EditorGUILayout.EnumPopup(new GUIContent("Align Mode", "Select how objects should be aligned along a specified axis."), alignMode);
 
-        if (GUILayout.Button(new GUIContent("Align to X Axis", "Align selected objects along the X axis")))
+        if (GUILayout.Button(new GUIContent("Align to X Axis", "Align selected objects along the X axis based on the chosen alignment mode (Center, Min, Max).")))
         {
             AlignObjects(Axis.X);
         }
 
-        if (GUILayout.Button(new GUIContent("Distribute Evenly", "Distribute selected objects evenly between the first and last selected objects")))
+        if (GUILayout.Button(new GUIContent("Distribute Evenly", "Distribute selected objects evenly between the first and last selected objects.")))
         {
             DistributeObjects();
         }
@@ -78,14 +82,14 @@ public class CustomTransformTool : EditorWindow
         GUILayout.Space(10);
         EditorGUILayout.LabelField("Parent/Child Management", EditorStyles.boldLabel);
 
-        newParent = (GameObject)EditorGUILayout.ObjectField(new GUIContent("New Parent", "Set the new parent for selected objects"), newParent, typeof(GameObject), true);
+        newParent = (GameObject)EditorGUILayout.ObjectField(new GUIContent("New Parent", "Set the new parent for selected objects."), newParent, typeof(GameObject), true);
 
-        if (GUILayout.Button(new GUIContent("Reparent Selected Objects", "Set the parent of the selected objects to the specified object")))
+        if (GUILayout.Button(new GUIContent("Reparent Selected Objects", "Set the parent of the selected objects to the specified object.")))
         {
             ReparentObjects();
         }
 
-        if (GUILayout.Button(new GUIContent("Center Child to Parent", "Center the selected child objects to their respective parents")))
+        if (GUILayout.Button(new GUIContent("Center Child to Parent", "Center the selected child objects to their respective parents.")))
         {
             CenterChildToParent();
         }
@@ -93,7 +97,7 @@ public class CustomTransformTool : EditorWindow
         GUILayout.Space(10);
         EditorGUILayout.LabelField("Transform Presets", EditorStyles.boldLabel);
 
-        if (GUILayout.Button(new GUIContent("Save Preset", "Save the current transform settings as a preset")))
+        if (GUILayout.Button(new GUIContent("Save Preset", "Save the current transform settings as a preset.")))
         {
             SavePreset();
         }
@@ -113,7 +117,7 @@ public class CustomTransformTool : EditorWindow
         GUILayout.Space(10);
         EditorGUILayout.LabelField("Transform History", EditorStyles.boldLabel);
 
-        if (GUILayout.Button(new GUIContent("Save Transform State", "Save the current transform state of the selected objects")))
+        if (GUILayout.Button(new GUIContent("Save Transform State", "Save the current transform state of the selected objects.")))
         {
             SaveTransformState();
         }
@@ -133,8 +137,10 @@ public class CustomTransformTool : EditorWindow
         GUILayout.Space(10);
         EditorGUILayout.LabelField("Gizmo Customization", EditorStyles.boldLabel);
 
-        gizmoColor = EditorGUILayout.ColorField(new GUIContent("Gizmo Color", "Set the color of the gizmo"), gizmoColor);
-        gizmoSize = EditorGUILayout.FloatField(new GUIContent("Gizmo Size", "Set the size of the gizmo"), gizmoSize);
+        gizmoColor = EditorGUILayout.ColorField(new GUIContent("Gizmo Color", "Set the color of the gizmo in the Scene view."), gizmoColor);
+        gizmoSize = EditorGUILayout.FloatField(new GUIContent("Gizmo Size", "Set the size of the gizmo in the Scene view."), gizmoSize);
+
+        EditorGUILayout.EndScrollView(); // End scroll view
     }
 
     private void ApplyTransform()
@@ -149,7 +155,7 @@ public class CustomTransformTool : EditorWindow
             }
             else if (snapMode == SnapMode.Vertex)
             {
-                obj.transform.position = SnapToNearestVertex(position);
+                obj.transform.position = SnapToNearestVertex(position, obj.transform);
             }
             else if (snapMode == SnapMode.Edge)
             {
@@ -174,11 +180,14 @@ public class CustomTransformTool : EditorWindow
         );
     }
 
-    private Vector3 SnapToNearestVertex(Vector3 position)
+    private Vector3 SnapToNearestVertex(Vector3 position, Transform transformToIgnore)
     {
         Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
+            if (hit.transform == transformToIgnore)
+                return position; // Ignore snapping to self
+
             Mesh mesh = hit.collider.GetComponent<MeshFilter>().sharedMesh;
             Transform hitTransform = hit.collider.transform;
             Vector3 closestVertex = Vector3.zero;
@@ -261,40 +270,30 @@ public class CustomTransformTool : EditorWindow
     {
         if (Selection.gameObjects.Length < 2) return;
 
-        float value = 0f;
-        switch (alignMode)
-        {
-            case AlignMode.Center:
-                value = Selection.gameObjects[0].transform.position[(int)axis];
-                break;
-            case AlignMode.Min:
-                value = Mathf.Min(Selection.gameObjects.Select(obj => obj.transform.position[(int)axis]).ToArray());
-                break;
-            case AlignMode.Max:
-                value = Mathf.Max(Selection.gameObjects.Select(obj => obj.transform.position[(int)axis]).ToArray());
-                break;
-        }
-
+        float targetValue = Selection.gameObjects[0].transform.position[(int)axis];
         foreach (GameObject obj in Selection.gameObjects)
         {
             Undo.RecordObject(obj.transform, "Align Objects");
-            Vector3 position = obj.transform.position;
-            position[(int)axis] = value;
-            obj.transform.position = position;
+            Vector3 pos = obj.transform.position;
+            pos[(int)axis] = targetValue;
+            obj.transform.position = pos;
         }
     }
 
     private void DistributeObjects()
     {
-        if (Selection.gameObjects.Length < 2) return;
+        if (Selection.gameObjects.Length < 3) return;
 
-        Vector3 firstPosition = Selection.gameObjects[0].transform.position;
-        Vector3 lastPosition = Selection.gameObjects[Selection.gameObjects.Length - 1].transform.position;
+        Transform firstObj = Selection.gameObjects[0].transform;
+        Transform lastObj = Selection.gameObjects[Selection.gameObjects.Length - 1].transform;
+
+        float totalDistance = Vector3.Distance(firstObj.position, lastObj.position);
+        float step = totalDistance / (Selection.gameObjects.Length - 1);
 
         for (int i = 1; i < Selection.gameObjects.Length - 1; i++)
         {
             Undo.RecordObject(Selection.gameObjects[i].transform, "Distribute Objects");
-            Selection.gameObjects[i].transform.position = Vector3.Lerp(firstPosition, lastPosition, i / (float)(Selection.gameObjects.Length - 1));
+            Selection.gameObjects[i].transform.position = Vector3.Lerp(firstObj.position, lastObj.position, (float)i / (Selection.gameObjects.Length - 1));
         }
     }
 
@@ -363,12 +362,20 @@ public class CustomTransformTool : EditorWindow
         }
     }
 
-    private void OnDrawGizmos()
+    private void DrawGrid(Vector3 origin, float gridSize, int gridLines = 10)
     {
-        if (Selection.activeGameObject != null)
+        Handles.color = Color.gray;
+        int halfGridLines = gridLines / 2;
+
+        for (int i = -halfGridLines; i <= halfGridLines; i++)
         {
-            Gizmos.color = gizmoColor;
-            Gizmos.DrawWireCube(Selection.activeGameObject.transform.position, Vector3.one * gizmoSize);
+            Vector3 startPosX = origin + new Vector3(i * gridSize, 0, -halfGridLines * gridSize);
+            Vector3 endPosX = origin + new Vector3(i * gridSize, 0, halfGridLines * gridSize);
+            Handles.DrawLine(startPosX, endPosX);
+
+            Vector3 startPosZ = origin + new Vector3(-halfGridLines * gridSize, 0, i * gridSize);
+            Vector3 endPosZ = origin + new Vector3(halfGridLines * gridSize, 0, i * gridSize);
+            Handles.DrawLine(startPosZ, endPosZ);
         }
     }
 
@@ -378,24 +385,42 @@ public class CustomTransformTool : EditorWindow
         {
             Transform activeTransform = Selection.activeTransform;
 
+            if (snapMode == SnapMode.Grid && snapToGrid)
+            {
+                DrawGrid(activeTransform.position, gridSize);
+
+                // Snap to grid in real-time
+                Vector3 newPosition = SnapToGrid(activeTransform.position);
+                if (activeTransform.position != newPosition)
+                {
+                    Undo.RecordObject(activeTransform, "Grid Snap");
+                    activeTransform.position = newPosition;
+                    GUI.changed = true;
+                }
+            }
+
+            Vector3 previewPosition = activeTransform.position;
+
             if (snapMode == SnapMode.Vertex || snapMode == SnapMode.Edge)
             {
-                HandleUtility.AddDefaultControl(GUIUtility.GetControlID(FocusType.Passive));
-                Handles.color = Color.green;
-                Vector3 snappedPosition = activeTransform.position;
-
                 if (snapMode == SnapMode.Vertex)
                 {
-                    snappedPosition = SnapToNearestVertex(activeTransform.position);
+                    previewPosition = SnapToNearestVertex(activeTransform.position, activeTransform);
                 }
                 else if (snapMode == SnapMode.Edge)
                 {
-                    snappedPosition = SnapToNearestEdge(activeTransform.position);
+                    previewPosition = SnapToNearestEdge(activeTransform.position);
                 }
 
-                activeTransform.position = snappedPosition;
-                Handles.DrawWireCube(snappedPosition, Vector3.one * 0.1f);
+                Handles.color = Color.green;
+                Handles.DrawWireCube(previewPosition, Vector3.one * 0.1f);
                 SceneView.RepaintAll();
+
+                if (Event.current.type == EventType.MouseDown && Event.current.button == 0)
+                {
+                    activeTransform.position = previewPosition;
+                    Event.current.Use(); // Consume the event so it doesn't propagate
+                }
             }
         }
     }
